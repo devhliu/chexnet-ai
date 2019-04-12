60b4f4433d678b159153e09786156da7ff8b261c
Filemon Mateus <fmateus.17@westminster-mo.edu>
2019-04-12 09:08:49 +0100

    refactoring functions on chexnet v1.0

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,7 @@ use Braintree\Customer;
 use Illuminate\Http\Request;
 use App\Services\ActivationService;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
